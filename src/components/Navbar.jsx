3038b35e55b4964da7d8ffd621732e6d4518f4c7
ArpanSankesh <arpansankesh830@gmail.com>
2025-02-05 22:06:40 +0530

    ANIMATION

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { FaTimes, FaBars } from "react-icons/fa";
 import { LINKS } from "../constants/index";
 import { SOCIAL_MEDIA_LINKS } from "../constants/index";
+import { AnimatePresence , motion} from "framer-motion";
 
 const Navbar = () => {
 
@@ -21,6 +22,20 @@ const Navbar = () => {
 
   }, [isOpen])
 
+  const containerVariants = {
+    hidden: {opacity: 0, y:"-100%"},
+    visible:{opacity : 1, y:0, 
+      transition:{
+        staggerChildren:0.1
+      }
+    }
+  }
+
+  const linkVariants = {
+    hidden: {opacity: 0, y:-50},
+    visible:{opacity : 1, y:0,}
+  }
+
 
   return (
     <>
@@ -29,23 +44,34 @@ const Navbar = () => {
           {!isOpen ? <FaBars className=" w-6 h-6" /> : <FaTimes className=" w-6 h-6" />}
         </button>
       </nav>
-
+      <AnimatePresence>
       {isOpen && (
-        <div className="fixed inset-0 z-20 flex flex-col items-center justify-center bg-black text-white">
+        <motion.div
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        variants={containerVariants}
+         className="fixed inset-0 z-20 flex flex-col items-center justify-center bg-black text-white">
           <ul className="space-y-6 text-white">
             {LINKS.map((link) => (
-              <li key={link.id}>
+              <motion.li
+              // initial="hidden"
+              // animate="visible"
+              // exit="hidden"
+              variants={linkVariants}
+              key={link.id}>
                 <a href={`#${link.id}`} onClick={toggleMenu} className="text-5xl font-semibold uppercase tracking-wide hover:text-lime-300 lg:text-9xl">{link.name}</a>
-              </li>
+              </motion.li>
             ))}
           </ul>
           <div className="absolute bottom-5  mt-20 flex items-center justify-center gap-8 mb-10 ">
             {SOCIAL_MEDIA_LINKS.map((link, id) => (
-              <a key={id} href={link.href} target="_blank" rel="noopener noreferrer" className="hover:text-lime-300 ">{link.icon}</a>
+              <motion.a variants={linkVariants} key={id} href={link.href} target="_blank" rel="noopener noreferrer" className="hover:text-lime-300 ">{link.icon}</motion.a>
             ))}
           </div>
-        </div>
+        </motion.div>
       )}
+      </AnimatePresence>
 
     </>
   )
